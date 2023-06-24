8804c9acca18032b25e4d4a0f0c012b7bc648136
Crowdbotics <engineering@crowdbotics.com>
2023-06-24 23:51:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-feather-dev-94423",
+  url: "https://still-feather-dev-94423.botics.co",
+  api: "https://still-feather-dev-94423.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
